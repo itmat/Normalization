be5d56fcfc8da01d0d771ca0266eca54ab9d6aa1
Eun Ji Kim <eunji.june.kim@gmail.com>
2014-03-12 11:35:07 -0400

    mappingstats -qsub

--- norm_scripts/runall_sam2mappingstats.pl
+++ norm_scripts/runall_sam2mappingstats.pl
@@ -1,5 +1,5 @@
 if(@ARGV < 4) {
-    die "Usage: perl runall_sam2mappingstats.pl <sample dir> <loc> <sam file name> <total_num_reads?>
+    die "Usage: perl runall_sam2mappingstats.pl <sample dir> <loc> <sam file name> <total_num_reads?> [options]
 
 <sample dir> is a file with the names of the sample directories. 
 
@@ -16,6 +16,33 @@ if(@ARGV < 4) {
 
 SAM file must use the IH or NH tags to indicate multi-mappers
 
+option:  -bsub : set this if you want to submit batch jobs to LSF.
+
+         -qsub : set this if you want to submit batch jobs to Sun Grid Engine.
+
+";
+}
+$bsub = "false";
+$qsub = "false";
+$numargs = 0;
+for ($i=4; $i<@ARGV; $i++){
+    $option_found = "false";
+    if ($ARGV[$i] eq '-bsub'){
+	$bsub = "true";
+	$numargs++;
+	$option_found = "true";
+    }
+    if ($ARGV[$i] eq '-qsub'){
+	$qsub = "true";
+	$numargs++;
+	$option_found = "true";
+    }
+    if ($option_found eq "false"){
+	die "option \"$ARGV[$i]\" was not recognized.\n";
+    }
+}
+if($numargs ne '1'){
+    die "you have to specify how you want to submit batch jobs. choose either -bsub or -qsub.\n
 ";
 }
 
@@ -26,8 +53,7 @@ $sampledirs = $ARGV[0];
 $LOC = $ARGV[1];
 $LOC =~ s/\/$//;
 @fields = split("/", $LOC);
-$size = @fields;
-$last_dir = $fields[@size-1];
+$last_dir = $fields[@fields-1];
 $study_dir = $LOC;
 $study_dir =~ s/$last_dir//;
 $shdir = $study_dir . "shell_scripts";
@@ -52,12 +78,17 @@ if ($total_reads_file eq "true"){
 	$shfile = "$shdir/m." . $id . "runsam2mappingstats.sh";
 	open(OUTFILE, ">$shfile");
 	print OUTFILE "perl $path $LOC/$dir/$sam_name -numreads $num_id > $LOC/$dir/$id.mappingstats.txt\n";
+    	close(OUTFILE);
+	if ($bsub eq "true"){
+	    `bsub -q max_mem30 -o $logdir/$id.sam2mappingstats.out -e $logdir/$id.sam2mappingstats.err sh $shfile`;
 	}
-	close(OUTFILE);
-	`bsub -q max_mem30 -o $logdir/$id.sam2mappingstats.out -e $logdir/$id.sam2mappingstats.err sh $shfile`;
+	if ($qsub eq "true"){
+	    `qsub -cwd -N $dir.sam2mappingstats -o $logdir -e $logdir -l h_vmem=15G $shfile`;
+	}
     }
-    close(INFILE);
 }
+close(INFILE);
+
 
 if ($total_reads_file eq "false"){
     open(INFILE, $sampledirs);
@@ -71,7 +102,13 @@ if ($total_reads_file eq "false"){
 	open(OUTFILE, ">$shfile");
 	print OUTFILE "perl $path $LOC/$dir/$sam_name > $LOC/$dir/$id.mappingstats.txt\n";
 	close(OUTFILE);
-	`bsub -q max_mem30 -o $logdir/$id.sam2mappingstats.out -e $logdir/$id.sam2mappingstats.err sh $shfile`;
+	if ($bsub eq "true"){
+	    `bsub -q max_mem30 -o $logdir/$id.sam2mappingstats.out -e $logdir/$id.sam2mappingstats.err sh $shfile`;
+	}
+	if ($qsub eq "true"){
+	    `qsub -cwd -N $dir.sam2mappingstats -o $logdir -e $logdir -l h_vmem=15G $shfile`;
+	}
     }
-    close(INFILE);
 }
+close(INFILE);
+
