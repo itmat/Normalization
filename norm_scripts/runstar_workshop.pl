ae4e95c6c9e79143b51c77ed0aa814153dfa60d0
Eun Ji Kim <eunji.june.kim@gmail.com>
2014-03-16 22:15:33 -0400

    runstar

---
+++ norm_scripts/runstar_workshop.pl
@@ -0,0 +1,36 @@
+if(@ARGV<2) {
+    die "usage: runstar_workshop.pl <sample dir> <loc> 
+
+where:
+<sample dir> is the name of a file with the names of sample directories (no paths)
+<loc> is the path to the dir with the sample directories
+
+"}
+
+$LOC = $ARGV[1];
+$LOC =~ s/\/$//;
+@fields = split("/", $LOC);
+$last_dir = $fields[@fields-1];
+$study_dir = $LOC;
+$study_dir =~ s/$last_dir//;
+$shdir = $study_dir . "shell_scripts";
+$logdir = $study_dir . "logs";
+unless (-d $shdir){
+    `mkdir $shdir`;}
+unless (-d $logdir){
+    `mkdir $logdir`;}
+
+open(INFILE, $ARGV[0]); #sample directories
+while($line = <INFILE>){
+    chomp($line);
+    $shfile = "$shdir/$line.runstar.sh";
+    open(OUT, ">$shfile");
+    print OUT "STAR --outFileNamePrefix $LOC/$line/ --genomeDir $LOC/star_chr1and2 --outSAMunmapped Within --readFilesIn $LOC/$line/forward_"."$line.fq $LOC/$line/reverse_"."$line.fq\n";
+    close(OUT);
+    `bsub -o $logdir/$line.star.out -e $logdir/$line.star.err sh $shfile`;
+}
+close(INFILE); 
+
+
+
+
