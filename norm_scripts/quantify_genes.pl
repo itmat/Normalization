78d39c0515cd5ceba1a6828a8da6f7812367bb35
Eun Ji Kim <eunji.june.kim@gmail.com>
2014-09-10 11:22:11 -0400

    fixed bug in quant_genes.pl

--- norm_scripts/quantify_genes.pl
+++ norm_scripts/quantify_genes.pl
@@ -10,7 +10,7 @@ my $USAGE = "\nUsage: perl quantify_genes.pl <sam2genes output> <genes file> <ou
 
 \n";
 
-if (@ARGV<2){
+if (@ARGV<3){
     die $USAGE;
 }
 
@@ -18,7 +18,7 @@ my $input = $ARGV[0];
 my $genes = $ARGV[1];
 my $outfile = $ARGV[2];
 
-my (%GENE, %GENE_COORDS, %GENE_counts_min, %GENE_counts_max, %READ_count, %READ_U, %READ_NU);
+my (%GENE, %GENE_COORDS, %GENE_counts_min, %GENE_counts_max, %READ_NU);
 open(IN, $genes);
 my $header = <IN>;
 while(my $line = <IN>){
@@ -34,16 +34,6 @@ while(my $line = <IN>){
 }
 close(IN);
 
-open(INFILE1, $input);
-$header = <INFILE1>;
-while(my $line = <INFILE1>){
-    chomp($line);
-    my @a = split(/\t/, $line);
-    my $read_id = $a[0];
-    $READ_count{$read_id}++;
-}
-close(INFILE1);
-
 open(INFILE2, $input);
 $header = <INFILE2>;
 while(my $line = <INFILE2>){
@@ -52,14 +42,14 @@ while(my $line = <INFILE2>){
     my $read_id = $a[0];
     my $gene_ids = $a[2];
     my $gene_syms = $a[3];
+    my $gene_tag = $a[5];
     if ($gene_ids =~ /^$/){
 	next;
     }
     my @b = split(",", $gene_ids);
     if (@b == 1){ #single gene id
-	if ($READ_count{$read_id} == 1){ #unique mapper
+	if ($gene_tag == 1){ #unique mapper
 	    $GENE_counts_min{$gene_ids}++;
-	    push (@{$READ_U{$read_id}}, $gene_ids);
 #	    print "$line\nUNIQUE\tONEGENE\tMIN\t$gene_ids\n";
 	}
 	else{ #non-unique mapper
@@ -72,10 +62,6 @@ while(my $line = <INFILE2>){
 		    }
 		}
 	    }
-	    if ($check == 1){
-		push (@{$READ_NU{$read_id}}, $gene_ids);
-#		print "$line\nNU\tONEGENE\tSKIP\t$gene_ids\n";
-	    }
 	    if ($check == 0){
 		$GENE_counts_max{$gene_ids}++;
 		push (@{$READ_NU{$read_id}}, $gene_ids);
@@ -84,26 +70,10 @@ while(my $line = <INFILE2>){
 	}
     }
     else{ #multiple gene ids
-	if ($READ_count{$read_id} == 1){ #unique mapper
+	if ($gene_tag == 1){ #unique mapper
 	    for(my $i=0;$i<@b;$i++){
-		my $check = 0;
-		#if gene id exists in the $READ{$read_id} array, do not increment counter
-		if (exists $READ_U{$read_id}){
-		    for (my $j=0; $j<@{$READ_U{$read_id}}; $j++){
-			if ($READ_U{$read_id}[$j] eq $b[$i]){
-			    $check = 1;
-			}
-		    }
-		}
-		if ($check == 1){
-		    push (@{$READ_U{$read_id}}, $b[$i]);
-#		    print "$line\nUNIQUE\tMULTGENE\tSKIP\t$b[$i]\n";
-		}
-		if ($check == 0){
-		    $GENE_counts_max{$b[$i]}++;
-		    push (@{$READ_U{$read_id}}, $b[$i]);
+		$GENE_counts_max{$b[$i]}++;
 #		    print "$line\nUNIQUE\tMULTGENE\tMAX\t$b[$i]\n";
-		}
 	    }
 	}
 	else{ #non-unique read
@@ -117,10 +87,6 @@ while(my $line = <INFILE2>){
                         }
                     }
 		}
-                if ($check == 1){
-                    push (@{$READ_NU{$read_id}}, $b[$i]);
-#		    print "$line\nNON-UNIQUE\tMULTGENE\tSKIP\t$b[$i]\n";
-                }
                 if ($check == 0){
                     $GENE_counts_max{$b[$i]}++;
                     push (@{$READ_NU{$read_id}}, $b[$i]);
